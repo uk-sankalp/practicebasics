b667c7bf1a6c36ce1158741907988a98b7d823c7
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-04-20 23:09:15 +0530

    Update User_Authentication .java

--- User_Authentication .java
+++ User_Authentication .java
@@ -7,9 +7,9 @@ public class User_Authentication
         String username, password;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter username:");
-        username = s.nextLine();
+        username = s.nextLine();//user name is stored
         System.out.print("Enter password:");
-        password = s.nextLine();
+        password = s.nextLine();//here comes password
         if(username.equals("user") && password.equals("user"))
         {
             System.out.println("Authentication Successful");
