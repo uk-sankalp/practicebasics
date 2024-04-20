bd0d77da6ae82958683a6551c1cfb989465c5fd5
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-04-20 23:06:13 +0530

    Update and rename New2.java to User_Authentication .java

---
+++ User_Authentication .java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class User_Authentication 
+{
+    public static void main(String args[])
+    {
+        String username, password;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter username:");
+        username = s.nextLine();
+        System.out.print("Enter password:");
+        password = s.nextLine();
+        if(username.equals("user") && password.equals("user"))
+        {
+            System.out.println("Authentication Successful");
+        }
+        else
+        {
+            System.out.println("Authentication Failed");
+        }
+    }
+}
+    }
+}
