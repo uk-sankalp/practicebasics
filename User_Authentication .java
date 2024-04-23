8c8497b4caae5db07e4d848695a5107b69485091
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-04-24 01:25:07 +0530

    Update User_Authentication .java

--- User_Authentication .java
+++ User_Authentication .java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class User_Authentication 
 {
-    public static void main(String args[])
+    public static void main(String args[])//program won't run if we dont a
     {
         String username, password;
         Scanner s = new Scanner(System.in);
